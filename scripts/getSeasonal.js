2ebf537ce752b9f82f1415ca0616276e328a0d94
Elkin Romero <gio.busrom@gmail.com>
2020-04-17 21:39:58 -0600

    Add functionality to the Seasonal module

--- scripts/getSeasonal.js
+++ scripts/getSeasonal.js
@@ -16,27 +16,27 @@ var endpoint="https://tethys2.byu.edu/localsptapi/api/"
 //*********SEASONAL AVERAGE FUNCTION TAB*******////
 module.exports={
   graph_s:function(reachid,htmlELement,add,width,height) {
-  width = (typeof width !== 'undefined') ?  width : 500;
-  height = (typeof heigth !== 'undefined') ?  heigth : 500;
-  add = (typeof width !== 'undefined') ?  width : false;
-  console.log('WE HAVE ENTERED GET_SEASONAL_AVERAGE FUNCTION()');
-  var layer_URL=endpoint+"SeasonalAverage/?reach_id="+reachid+"&return_format=csv";
-  $.ajax({
-    type:'GET',
-    url: layer_URL,
-    dataType: 'text',
-    contentType:'text/plain',
-    success: function(data) {
+    width = (typeof width !== 'undefined') ?  width : 500;
+    height = (typeof heigth !== 'undefined') ?  heigth : 500;
+    add = (typeof add !== 'undefined') ?  add : false;
+    console.log('WE HAVE ENTERED GET_SEASONAL_AVERAGE FUNCTION()');
+    var layer_URL=endpoint+"SeasonalAverage/?reach_id="+reachid+"&return_format=csv";
+    $.ajax({
+      type:'GET',
+      url: layer_URL,
+      dataType: 'text',
+      contentType:'text/plain',
+      success: function(data) {
 
-      var allLines = data.split('\n');
-      var headers = allLines[0].split(',');
+        var allLines = data.split('\n');
+        var headers = allLines[0].split(',');
 
-      for (var i=1; i < allLines.length; i++) {
-        var data = allLines[i].split(',');
-        dates.dates.push(data[0]);
-        values.mean.push(data[1]);
-      }
-    },
+        for (var i=1; i < allLines.length; i++) {
+          var data = allLines[i].split(',');
+          dates.dates.push(data[0]);
+          values.mean.push(data[1]);
+        }
+      },
       complete: function() {
           console.log("complete part of the ajax call for getSeasonalAverage");
           var mean = {
@@ -49,36 +49,25 @@ module.exports={
           var data = [mean];
 
           var layout = {
+              autosize: true,
+
               // title: 'Historical Streamflow<br>'+titleCase(watershed) + ' Reach ID:' + comid,
               title: 'Seasonal Average StreamFlow<br>'+' Reach ID:' + reachid,
-              xaxis: {title: 'Date'},
-              yaxis: {title: 'Streamflow m3/s', range: [0, Math.max(...data[0].y) + Math.max(...data[0].y)/5]},
+              width: width,
+              height: height,
+              xaxis: {title: 'Date',showgrid: false},
+              yaxis: {title: 'Streamflow m3/s',showgrid: false},
               // plot_bgcolor:"#7782c5",
 
               //shapes: returnShapes,
           }
-          //Removing any exisisting element with the same name//
-          var divELement=document.getElementById("graph");
-          if(divELement != null && add==false){
-            Plotly.purge(divELement);
-            divELement.remove();
-          };
 
-          divELement=document.createElement('div');
-          divELement.setAttribute("id", "graph");
-          divELementParent= document.getElementById(htmlELement);
-          divELementParent.append(divELement);
-
-          Plotly.newPlot('graph', data, layout);
+          Plotly.purge(htmlELement);
+          Plotly.newPlot(htmlELement, data, layout);
 
           var index = data[0].x.length-1;
 
-          // for(var i=0; i<data[0].y.length;++i){
-          //   console.log(i);
-          //   console.log(data[0].y[i]);
-          //
-          // };
-          returnPeriods.graph_rp(reachid, data[0].x[0], data[0].x[index],width,height);
+          returnPeriods.graph_rp(reachid, data[0].x[0], data[0].x[index],width,height,htmlELement);
 
           // getreturnperiods(reachid, data[0].x[0], data[0].x[index],width,height);
 
@@ -89,7 +78,6 @@ module.exports={
     });
   },
   downloadData:function(reachid){
-    console.log("enter the download function");
     // THIS IS A FUNCTION TO DOWNLOAD DATA //
     var downloadUrl=endpoint+"SeasonalAverage/?reach_id="+reachid+"&return_format=csv";
     // var downloadUrl="https://tethys2.byu.edu/localsptapi/api/"+"ForecastEnsembles/?reach_id="+reachid+"&return_format=csv";
@@ -100,7 +88,6 @@ module.exports={
     // req.setRequestHeader('my-custom-header', 'custom-value'); // adding some headers (if needed)
 
     req.onload = function (event) {
-      console.log("onload");
       var blob = req.response;
       var fileName = reachid;
       var contentType = req.getResponseHeader("content-type");
@@ -118,7 +105,6 @@ module.exports={
 
       if (window.navigator.msSaveOrOpenBlob) {
         // Internet Explorer
-        console.log("enter second if ");
         window.navigator.msSaveOrOpenBlob(new Blob([blob], {type: contentType}), fileName);
       }
       else {
@@ -133,5 +119,78 @@ module.exports={
       }
     };
     req.send();
+  },
+  locationGraph_s: function(lat,lon,htmlELement,add, width,height){
+    width = (typeof width !== 'undefined') ?  width : 500;
+    height = (typeof heigth !== 'undefined') ?  heigth : 500;
+    add = (typeof add !== 'undefined') ?  add : false;
+
+    var layer_URL1=endpoint +"GetReachID/?lat="+lat+"&long="+lon;
+    $.ajax({
+      type: 'GET',
+      url: layer_URL1,
+      success: function(data) {
+        var dataText=JSON.stringify(data);
+        var reachid=dataText['reach_id'];
+        var layer_URL=endpoint+"SeasonalAverage/?reach_id="+reachid+"&return_format=csv";
+        $.ajax({
+          type:'GET',
+          url: layer_URL,
+          dataType: 'text',
+          contentType:'text/plain',
+          success: function(data) {
+
+            var allLines = data.split('\n');
+            var headers = allLines[0].split(',');
+
+            for (var i=1; i < allLines.length; i++) {
+              var data = allLines[i].split(',');
+              dates.dates.push(data[0]);
+              values.mean.push(data[1]);
+            }
+          },
+            complete: function() {
+                console.log("complete part of the ajax call for getSeasonalAverage");
+                var mean = {
+                    name: 'Mean',
+                    x: dates.dates,
+                    y: values.mean,
+                    mode: "lines",
+                    line: {color: 'blue'}
+                };
+                var data = [mean];
+
+                var layout = {
+                    // title: 'Historical Streamflow<br>'+titleCase(watershed) + ' Reach ID:' + comid,
+                    title: 'Seasonal Average StreamFlow<br>'+' Reach ID:' + reachid,
+                    xaxis: {title: 'Date', showgrid: false},
+                    yaxis: {title: 'Streamflow m3/s', range: [0, Math.max(...data[0].y) + Math.max(...data[0].y)/5], showgrid: false},
+                    // plot_bgcolor:"#7782c5",
+
+                    //shapes: returnShapes,
+                }
+                //Removing any exisisting element with the same name//
+                Plotly.purge(htmlELement);
+                Plotly.newPlot(htmlELement, data, layout);
+
+                var index = data[0].x.length-1;
+
+                // for(var i=0; i<data[0].y.length;++i){
+                //   console.log(i);
+                //   console.log(data[0].y[i]);
+                //
+                // };
+                returnPeriods.graph_rp(reachid, data[0].x[0], data[0].x[index],width,height);
+
+                // getreturnperiods(reachid, data[0].x[0], data[0].x[index],width,height);
+
+                dates.highres = [], dates.dates = [];
+                values.highres = [], values.max = [], values.mean = [], values.min = [], values.std_dev_range_lower = [], values.std_dev_range_upper = [];
+            }//add lines to plotly
+
+          });
+        }
+    });
   }
+
 }
