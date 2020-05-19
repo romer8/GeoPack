8695f5e05bcac2f8c73575d1d676d1ce556c3a90
Elkin Romero <gio.busrom@gmail.com>
2020-05-19 16:55:50 -0600

    Addition of the returnPeriods function

---
+++ scripts/returnPeriods.js
@@ -0,0 +1,130 @@
+/**This Module only exports a graph containing historical data,
+it can be added with more functions if neeeded, porobably something great
+will be to have the coordinates of lat long available to makeable to plot a graph.
+**/
+
+//*** REQUIRE LIBRARIES***///
+var Plotly = require('plotly.js-dist');
+var $ = require("jquery");
+//**GLOBAL VARIABLES TO DEAL WITH THE FUNCTIONS**//
+// var dates = {highres: [], dates: []};
+// var values = {highres: [], max: [], mean: [], min: [], std_dev_range_lower: [], std_dev_range_upper: []};
+var returnShapes;
+var endpoint="http://0.0.0.0:8090/api/";
+
+
+module.exports= {
+ graph_rp: function (reachid,htmlELement,width, height) {
+ // var layer_URL="https://tethys2.byu.edu/localsptapi/api/ReturnPeriods/?reach_id="+reachid+"&return_format=csv";
+ var layer_URL=endpoint+"ReturnPeriods/?reach_id="+reachid+"&return_format=json";
+ console.log("inside getreturnperiods");
+   $.ajax({
+     type:'GET',
+     assync: true,
+     url: layer_URL,
+     dataType: 'json',
+     contentType:'application/json',
+     success: function (data) {
+       console.log(data);
+       var returnPeriodsObject = data['return_periods'];
+       var startDate = data['startdate'].split('T')[0];
+       var endDate = data['enddate'].split('T')[0];
+       var band_alt_max = -9999
+       var rp2 = returnPeriodsObject['return_period_2'];
+       var rp5 = returnPeriodsObject['return_period_5'];
+       var rp10 = returnPeriodsObject['return_period_10'];
+       var rp25 = returnPeriodsObject['return_period_25'];
+       var rp50 = returnPeriodsObject['return_period_50'];
+       var rp100 = returnPeriodsObject['return_period_100'];
+       console.log(rp100);
+       console.log(startDate);
+       console.log(endDate);
+       var shapes = [
+               // return 2 band
+               {
+                   type: 'rect',
+                   layer: 'below',
+                   xref: 'x',
+                   yref: 'y',
+                   x0: startDate,
+                   y0: rp2,
+                   x1: endDate,
+                   y1: rp5,
+                   line: {width:1},
+                   fillcolor: 'rgba(128, 255, 0, 0.4)'
+               },
+               // RETURN PERIOD 5 BAND
+               {
+                   type: 'rect',
+                   layer: 'below',
+                   xref: 'x',
+                   yref: 'y',
+                   x0: startDate,
+                   y0: rp5,
+                   x1: endDate,
+                   y1: rp10,
+                   line: {width:1},
+                   fillcolor: 'rgba(255, 255, 0, 0.4)'
+               },
+               // return 10 band
+               {
+                   type: 'rect',
+                   layer: 'below',
+                   xref: 'x',
+                   yref: 'y',
+                   x0: startDate,
+                   y0: rp10,
+                   x1: endDate,
+                   y1: rp25,
+                   line: {width: 1},
+                   fillcolor: 'rgba(255, 128, 0, 0.4)'
+               },
+               // return 25 band
+               {
+                   type: 'rect',
+                   layer: 'below',
+                   xref: 'x',
+                   yref: 'y',
+                   x0: startDate,
+                   y0: rp25,
+                   x1: endDate,
+                   y1: rp50,
+                   line: {width: 1},
+                   fillcolor: 'rgba(255, 0, 0, 0.4)'
+               },
+               // return 50 band
+               {
+                   type: 'rect',
+                   layer: 'below',
+                   xref: 'x',
+                   yref: 'y',
+                   x0: startDate,
+                   y0: rp50,
+                   x1: endDate,
+                   y1: rp100,
+                   line: {width: 1},
+                   fillcolor: 'rgba(255, 0, 255, 0.4)'
+               },
+
+               //return 100 band
+               {
+                 type: 'rect',
+                 layer: 'below',
+                 xref: 'x',
+                 yref: 'y',
+                 x0: startDate,
+                 y0: rp100,
+                 x1: endDate,
+                 y1: Math.max(rp100, band_alt_max),
+                 line: {width: 1},
+                 fillcolor: 'rgba(127, 0, 255, 0.4)'
+               },
+             ];
+         var update = {
+             shapes: shapes,
+         };
+         Plotly.relayout(htmlELement, update);
+       }
+   })
+ }
+}// create boxes for graph
