15d559ba9c748b0daa7fe460868f1651f69be77f
Elkin Romero <gio.busrom@gmail.com>
2020-05-21 17:43:26 -0600

    Add the abiity to download for the ensembles

--- scripts/DownloadAbility.js
+++ scripts/DownloadAbility.js
@@ -29,5 +29,70 @@ module.exports={
         } }],
     }
     return config;
+  },
+  addConfigObject : function(xObject,title_graph){
+    var config = {
+      modeBarButtonsToAdd: [{ name: 'downloadCsv', title: 'Download data as csv', icon: Plotly.Icons.disk, click: function(){
+        var csvData = [];
+        var header = Object.keys(xObject); //main header.
+        var header2 =Object.keys(xObject);
+        var indexDateTime= header.indexOf("datetime");
+        var indexDateTimeHighRes = header.indexOf("datetime_high_res");
+        if(indexDateTimeHighRes > -1){
+          header.splice(indexDateTimeHighRes,1);
+          var indexValuesHighRes = header.indexOf("ensemble_52_m^3/s");
+          header.splice(indexValuesHighRes,1);
+          header.push(header2[indexDateTimeHighRes]);
+          header.push(header2[header2.indexOf("ensemble_52_m^3/s")]);
+          csvData.push(header);
+          console.log("this is the header");
+          console.log(header);
+          console.log(xObject);
+          for (var i = 0; i < xObject[`ensemble_52_m^3/s`].length; i++){ //data
+            var line = [];
+            header.forEach(function(x){
+              console.log(xObject[`${x}`]);
+              if(xObject[`${x}`].length > i ){
+                line.push(xObject[`${x}`][i]);
+              }
+              else{
+                line.push(xObject[`${x}`][i]);
+              }
+            })
+            csvData.push(line);
+          }
+        }
+        else{
+          csvData.push(header);
+          for (var i = 0; i < xObject[`${header[0]}`].length; i++){ //data
+            var line = [];
+            header.forEach(function(x){
+              console.log(xObject[`${x}`]);
+              if(xObject[`${x}`].length > i ){
+                line.push(xObject[`${x}`][i]);
+              }
+              else{
+                line.push(xObject[`${x}`][i]);
+              }
+            })
+            csvData.push(line);
+          }
+
+        }
+
+        var csvFile = csvData.map(e=>e.map(a=>'"'+((a||"").toString().replace(/"/gi,'""'))+'"').join(",")).join("\r\n"); //quote all fields, escape quotes by doubling them.
+        var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+        var link = document.createElement("a");
+        var url = URL.createObjectURL(blob);
+        link.setAttribute("href", url);
+        link.setAttribute("download", title_graph.replace(/[^a-z0-9_.-]/gi,'_') + ".csv");
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        } }],
+    }
+    return config;
   }
+
 }
