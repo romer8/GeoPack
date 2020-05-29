dab9e6c57e8fe065148cbfa74893a005d4342846
Elkin Romero <gio.busrom@gmail.com>
2020-05-29 12:23:05 -0600

    Change return Periods completely, so it is easy to turn on and off the legends, so the ajax calls are done inside the historical/forecast function now

--- scripts/returnPeriods.js
+++ scripts/returnPeriods.js
@@ -8,122 +8,150 @@ var Plotly = require('plotly.js-dist');
 var $ = require("jquery");
 
 //**GLOBAL VARIABLES TO DEAL WITH THE FUNCTIONS**//
-var returnShapes;
 var endpoint="http://0.0.0.0:8090/api/";
 
 
 module.exports= {
- graph: function (reachid,htmlELement,width, height) {
- // var layer_URL="https://tethys2.byu.edu/localsptapi/api/ReturnPeriods/?reach_id="+reachid+"&return_format=csv";
- var layer_URL=endpoint+"ReturnPeriods/?reach_id="+reachid+"&return_format=json";
- console.log("inside getreturnperiods");
-   $.ajax({
-     type:'GET',
-     assync: true,
-     url: layer_URL,
-     dataType: 'json',
-     contentType:'application/json',
-     success: function (data) {
-       console.log(data);
-       var returnPeriodsObject = data['return_periods'];
-       var startDate = data['startdate'].split('T')[0];
-       var endDate = data['enddate'].split('T')[0];
-       var band_alt_max = -9999
-       var rp2 = returnPeriodsObject['return_period_2'];
-       var rp5 = returnPeriodsObject['return_period_5'];
-       var rp10 = returnPeriodsObject['return_period_10'];
-       var rp25 = returnPeriodsObject['return_period_25'];
-       var rp50 = returnPeriodsObject['return_period_50'];
-       var rp100 = returnPeriodsObject['return_period_100'];
-       console.log(rp100);
-       console.log(startDate);
-       console.log(endDate);
-       var shapes = [
-               // return 2 band
-               {
-                   type: 'rect',
-                   layer: 'below',
-                   xref: 'x',
-                   yref: 'y',
-                   x0: startDate,
-                   y0: rp2,
-                   x1: endDate,
-                   y1: rp5,
-                   line: {width:1},
-                   fillcolor: 'rgba(128, 255, 0, 0.4)',
-               },
-               // RETURN PERIOD 5 BAND
-               {
-                   type: 'rect',
-                   layer: 'below',
-                   xref: 'x',
-                   yref: 'y',
-                   x0: startDate,
-                   y0: rp5,
-                   x1: endDate,
-                   y1: rp10,
-                   line: {width:1},
-                   fillcolor: 'rgba(255, 255, 0, 0.4)'
-               },
-               // return 10 band
-               {
-                   type: 'rect',
-                   layer: 'below',
-                   xref: 'x',
-                   yref: 'y',
-                   x0: startDate,
-                   y0: rp10,
-                   x1: endDate,
-                   y1: rp25,
-                   line: {width: 1},
-                   fillcolor: 'rgba(255, 128, 0, 0.4)'
-               },
-               // return 25 band
-               {
-                   type: 'rect',
-                   layer: 'below',
-                   xref: 'x',
-                   yref: 'y',
-                   x0: startDate,
-                   y0: rp25,
-                   x1: endDate,
-                   y1: rp50,
-                   line: {width: 1},
-                   fillcolor: 'rgba(255, 0, 0, 0.4)'
-               },
-               // return 50 band
-               {
-                   type: 'rect',
-                   layer: 'below',
-                   xref: 'x',
-                   yref: 'y',
-                   x0: startDate,
-                   y0: rp50,
-                   x1: endDate,
-                   y1: rp100,
-                   line: {width: 1},
-                   fillcolor: 'rgba(255, 0, 255, 0.4)'
-               },
+ graph_rp: function (response_timeSeries,returnPeriodsObject,data_array) {
+   var nrp = {
+     name:'2-yr Return Period',
+     x: response_timeSeries['datetime'],
+     y: Array(response_timeSeries['datetime'].length).fill(returnPeriodsObject['return_period_2']),
+     mode: "lines",
+     fill:'none',
+     showlegend:false,
+     hoverinfo:'name+y',
+     legendgroup:'rp2',
+     line: {color: 'rgba(128, 255, 0, 0.4)',width: 1}
+   }
 
-               //return 100 band
-               {
-                 type: 'rect',
-                 layer: 'below',
-                 xref: 'x',
-                 yref: 'y',
-                 x0: startDate,
-                 y0: rp100,
-                 x1: endDate,
-                 y1: Math.max(rp100, band_alt_max),
-                 line: {width: 1},
-                 fillcolor: 'rgba(127, 0, 255, 0.4)'
-               },
-             ];
-         var update = {
-             shapes: shapes,
-         };
-         Plotly.relayout(htmlELement, update);
-       }
-   })
+   var rp2= {
+     name: '2-5yr Return Period',
+     x: response_timeSeries['datetime'],
+     y: Array(response_timeSeries['datetime'].length).fill(returnPeriodsObject['return_period_5']),
+     mode: "lines",
+     fill:'tonexty',
+     showlegend: true,
+     hoverinfo:'skip',
+     legendgroup:'rp2',
+     line: {color: 'rgba(128, 255, 0, 0.4)', width: 0}
+   }
+   var nrp2 = {
+     name:'5-yr Return Period',
+     x: response_timeSeries['datetime'],
+     y: Array(response_timeSeries['datetime'].length).fill(returnPeriodsObject['return_period_5']+0.1),
+     mode: "lines",
+     fill:'none',
+     showlegend: false,
+     hoverinfo:'name+y',
+     legendgroup:'rp5',
+     line: {color: 'rgba(253, 154, 1,.4)',width: 1}
+   }
+   var rp5 = {
+     name: '5-10yr Return Period',
+     x: response_timeSeries['datetime'],
+     y: Array(response_timeSeries['datetime'].length).fill(returnPeriodsObject['return_period_10']),
+     mode: "lines",
+     fill:'tonexty',
+     showlegend: true,
+     legendgroup:'rp5',
+     hoverinfo:'none',
+     line: {color: 'rgba(253, 154, 1, .4)', width: 0}
+   }
+
+   var nrp3 = {
+     name:'10-yr Return Period',
+     x: response_timeSeries['datetime'],
+     y: Array(response_timeSeries['datetime'].length).fill(returnPeriodsObject['return_period_10']+0.1),
+     mode: "lines",
+     fill:'none',
+     showlegend: false,
+     hoverinfo:'name+y',
+     legendgroup:'rp10',
+     line: {color: 'rgba(255, 56, 5, .4', width: 1}
+   }
+
+   var rp10 = {
+     name: '10-25yr Return Period',
+     x: response_timeSeries['datetime'],
+     y: Array(response_timeSeries['datetime'].length).fill(returnPeriodsObject['return_period_25']),
+     mode: "lines",
+     fill:'tonexty',
+     showlegend: true,
+     legendgroup:'rp10',
+     hoverinfo:'none',
+     line: {color: 'rgba(255, 56, 5, .4', width: 0}
+   }
+
+   var nrp4 = {
+     name:'25-yr Return Period',
+     x: response_timeSeries['datetime'],
+     y: Array(response_timeSeries['datetime'].length).fill(returnPeriodsObject['return_period_25']+0.1),
+     mode: "lines",
+     fill:'none',
+     showlegend: false,
+     hoverinfo:'name+y',
+     legendgroup:'rp25',
+     line: {color: 'rgba(255, 0, 0, .4)', width: 1}
+   }
+
+   var rp25 = {
+     name: '25-50yr Return Period',
+     x: response_timeSeries['datetime'],
+     y: Array(response_timeSeries['datetime'].length).fill(returnPeriodsObject['return_period_50']),
+     mode: "lines",
+     fill:'tonexty',
+     showlegend: true,
+     legendgroup:'rp25',
+     hoverinfo:'none',
+     line: {color: 'rgba(255, 0, 0, .4)', width: 0}
+   }
+
+   var nrp5 = {
+     name:'50-yr Return Period',
+     x: response_timeSeries['datetime'],
+     y: Array(response_timeSeries['datetime'].length).fill(returnPeriodsObject['return_period_50']+0.1),
+     mode: "lines",
+     fill:'none',
+     showlegend: false,
+     hoverinfo:'name+y',
+     legendgroup:'rp50',
+     line: {color: 'rgba(128, 0, 106, .4)', width: 1}
+   }
+
+   var rp50 = {
+     name: '50-100yr Return Period',
+     x: response_timeSeries['datetime'],
+     y: Array(response_timeSeries['datetime'].length).fill(returnPeriodsObject['return_period_100']),
+     mode: "lines",
+     fill:'tonexty',
+     showlegend: true,
+     legendgroup:'rp50',
+     hoverinfo:'none',
+     line: {color: 'rgba(128, 0, 106, .4)', width: 0}
+   }
+   var nrp6 = {
+     name:'100-yr Return Period',
+     x: response_timeSeries['datetime'],
+     y: Array(response_timeSeries['datetime'].length).fill(returnPeriodsObject['return_period_100']+0.1),
+     mode: "lines",
+     fill:'none',
+     showlegend: false,
+     hoverinfo:'name+y',
+     legendgroup:'rp100',
+     line: {color: 'rgba(128, 0, 106, .4)', width: 1}
+   }
+   data_array.push(nrp);
+   data_array.push(rp2);
+   data_array.push(nrp2);
+   data_array.push(rp5);
+   data_array.push(nrp3);
+   data_array.push(rp10);
+   data_array.push(nrp4);
+   data_array.push(rp25);
+   data_array.push(nrp5);
+   data_array.push(rp50);
+   data_array.push(nrp6);
+
  }
 }
